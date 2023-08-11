bb99ef78ab75ef1073a7434b8bed3baf325f7caa
El-Zieny <abdoelzieny72@gmail.com>
2023-08-12 00:14:00 +0300

    Task #4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for c in range(0,len(names)):
+    for c in range(0, len(names)):
         if names[c][:2] != "__":
             print(names[c])
