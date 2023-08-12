2398da50715de6973995fb106897a94907479016
El-Zieny <abdoelzieny72@gmail.com>
2023-08-12 17:30:40 +0300

    Task #0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 def print_list_integer(my_list=[]):
     x = len(my_list)
     for c in range(0, x):
-        print("{}".format(my_list[c]))
+        i = int(my_list[c])
+        print("{}".format(i))
