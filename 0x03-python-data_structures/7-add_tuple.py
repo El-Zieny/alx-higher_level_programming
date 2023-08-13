98c66e0793e964b64583768eef6dd7c9e6709707
El-Zieny <abdoelzieny72@gmail.com>
2023-08-13 07:42:33 +0300

    Task #7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(a=(), b=()):
+    if not len(a):
+        return b
+    if not len(b):
+        return a
+    if len(a) == 1 and len(b) == 1:
+        return (a[0] + b[0], 0)
+    if len(a) == 1 and len(b) == 2:
+        return (a[0] + b[0], b[1])
+    if len(a) == 2 and len(b) == 1:
+        return (a[0] + b[0], a[1])
+    x = a[0] + b[0]
+    y = a[1] + b[1]
+    return (x, y)
