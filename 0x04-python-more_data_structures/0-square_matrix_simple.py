90209474988afeecc6572e9afe1f84ab99c310f0
abdulrahman elzieny <abdoelzieny72@gmail.com>
2023-08-18 19:50:12 -0700

    Task #0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_Simple(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
-    return new_matrix
-#!/usr/bin/python3
-#def square_matrix_simple(matrix=[]):
-#  new = list
+def square_matrix_simple(matrix=[]):
+    new = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return new
