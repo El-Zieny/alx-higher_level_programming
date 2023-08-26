26d342c02f1120d5e3ac58e709db03cf04de3005
El-Zieny <abdoelzieny72@gmail.com>
2023-08-26 20:22:33 +0300

    Task #12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        t = i % 3
+        f = i % 5
+        if !t and !f:
+            print("FizzBuzz")
+        else if !t:
+            print("Fizz")
+        else if !f:
+            print("Buzz")
