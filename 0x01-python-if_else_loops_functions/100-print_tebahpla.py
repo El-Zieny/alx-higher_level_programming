26db0618645e0eb417e1f3cab1838079c5c16bfb
El-Zieny <abdoelzieny72@gmail.com>
2023-08-26 22:05:07 +0300

    Task #14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for x in range(26, 0, -1):
-        print("{}".format(chr(x + 64 if x % 2 else x + 96)), end="")
+    print("{}".format(chr(x + 64 if x % 2 else x + 96)), end="")
