3b64587bbfa7164484d2998f3b52004c66c4d041
El-Zieny <abdoelzieny72@gmail.com>
2023-08-30 02:17:48 +0300

    Task #2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Square class that validate size"""
+"""Square class that validate size"""
 
 
 class Square:
     """Square class"""
+
     def __init__(self, s=0):
         """class constructor"""
         if type(s) is not int:
             raise TypeError('size must be an integer')
         if s < 0:
             raise ValueError('size must be >= 0')
-            self.__size = s
+        self.__size = s
