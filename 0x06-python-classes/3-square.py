e8bec2a51f536d1454ca45b09f03ce519adf7c4a
El-Zieny <abdoelzieny72@gmail.com>
2023-08-30 02:27:22 +0300

    Task #3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,6 +6,7 @@ class Square:
     """Square class"""
 
     def __init__(self, s=0):
+        """class constructor"""
         if type(s) is not int:
             raise TypeError('size must be an integer')
         if s < 0:
