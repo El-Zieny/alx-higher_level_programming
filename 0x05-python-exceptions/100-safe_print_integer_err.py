ee02a3f70637bc26a5c2ed282175297888228f02
El-Zieny <abdoelzieny72@gmail.com>
2023-08-29 04:49:47 +0300

    Task #7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(val):
+    import stderr
+    try:
+        print("{:d}".format(val))
+        return True
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        return False
