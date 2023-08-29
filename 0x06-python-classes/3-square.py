daded5a714f362c0d92457e38d8ddd9ac3d351a6
El-Zieny <abdoelzieny72@gmail.com>
2023-08-30 02:28:54 +0300

    Task #3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
         if s < 0:
             raise ValueError('size must be >= 0')
         self.__size = s
+
     def area(self):
         """area of square"""
         return self.__size ** 2
