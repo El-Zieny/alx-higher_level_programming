c540b762db826d9a470c08b341aeada05981d2e8
El-Zieny <abdoelzieny72@gmail.com>
2023-08-30 02:26:49 +0300

    Task #3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class size and area"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, s=0):
+        if type(s) is not int:
+            raise TypeError('size must be an integer')
+        if s < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = s
+    def area(self):
+        return self.__size ** 2
--- 0x06-python-classes/vain.py
+++ 0x06-python-classes/vain.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-square = __import__('0-square').square
+Square = __import__('3-square').Square
 
-my_square = square()
-print(type(my_square))
-print(my_square.__dict__)
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
