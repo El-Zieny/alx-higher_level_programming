a8e14c5d55bf285ca0979b13734ff0a2e2e53967
El-Zieny <abdoelzieny72@gmail.com>
2023-09-05 01:59:50 +0300

    python: more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+
+class Rectangle:
+    """rectangle class"""
+    def __init__(self, w=0, h=0):
+        if type(w) is not int:
+            raise TypeError("width must be an integer")
+        if w < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = w
+        if type(h) is not int:
+            raise TypeError("height must be an integer")
+        if h < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = h
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if type(w) is not int:
+            raise TypeError("width must be an integer")
+        if w < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = w
+
+    @property
+    def width(self):
+        return self.__height
+
+    @width.setter
+    def width(self, h):
+        if type(h) is not int:
+            raise TypeError("height must be an integer")
+        if w < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = h
