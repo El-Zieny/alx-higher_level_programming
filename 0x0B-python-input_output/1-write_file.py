0f685296bfc967f57bcd813c446abf9b435fa862
El-Zieny <abdoelzieny72@gmail.com>
2023-09-13 02:48:27 +0300

    python|I/O

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """a function that overwrite a file or create it if it doesnt exist"""
     with open(filename, mode="w", encoding="utf-8") as wr:
         x = wr.write(text)
 
