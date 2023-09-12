e5c6f1b153e34423fd58ff279c25379354600aac
El-Zieny <abdoelzieny72@gmail.com>
2023-09-13 02:40:58 +0300

    functions|nested_loops

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,12 +3,6 @@
 
 
 def read_file(filename=""):
-    """
-    function to print the content of a filename to stdout
-    :param filename:
-    :type filename:
-    :return:
-    :rtype:
-    """
+    """function to print the content of a filename to stdout"""
     with open(filename, mode='r', encoding="utf-8") as file:
         print(file.read(), end="")
