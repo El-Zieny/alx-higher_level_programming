13a17a854c1b0bb2875cd02427042b473a8b1611
El-Zieny <abdoelzieny72@gmail.com>
2023-09-13 02:32:37 +0300

    functions|nested_loops

--- 0x0B-python-inout_output/0-read_file.py
+++ 0x0B-python-inout_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """function to print the content of a file to stdout"""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
