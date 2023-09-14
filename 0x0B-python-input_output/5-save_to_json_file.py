80bfd2a882959f60ca2a8108f693e109623ecd55
El-Zieny <abdoelzieny72@gmail.com>
2023-09-14 08:49:58 +0300

    py io

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,6 @@ def save_to_json_file(my_obj, filename):
     :return: string
     :rtype: str
     """
-    with open(filename, mode='w') as j:
+    with open(file=filename, mode='w') as j:
         from json import dump
         dump(my_obj, j)
