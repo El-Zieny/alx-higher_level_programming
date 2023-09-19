4f67806a6aae07e1d57e00bf385af23312b7dba3
abdulrahman elzieny <abdoelzieny72@gmail.com>
2023-09-18 18:29:40 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,14 @@ class Base:
             return dumps(list_dict)
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        lst = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for instance in list_objs:
+                lst.append(cls.to_dictionary(instance))
+
+        with open(filename, mode="w") as file:
+            file.write(Base.to_json_string(lst))
