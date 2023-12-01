04ca7064d3716208e9483dfc8e2f8c7c6cc7c3de
El-Zieny <abdoelzieny72@gmail.com>
2023-12-02 00:57:45 +0200

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- a script that creates a new table if not existing
-CREATE TABLE IF NOT EXIST first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
