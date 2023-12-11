43b552b54274c78db4a0be0071d7db25f5693019
El-Zieny <abdoelzieny72@gmail.com>
2023-12-12 00:54:49 +0200

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const { argv } = require('process');
-function add(a, b) {
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 const x = parseInt(argv[2]);
 const z = parseInt(argv[3]);
 if (isNaN(x) || isNaN(z)) {
-    console.log(NaN);
+  console.log(NaN);
 } else {
-    console.log(add(x, z));
+  console.log(add(x, z));
 }
