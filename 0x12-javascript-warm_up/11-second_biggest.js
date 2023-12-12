96a60caca14c569530a29a12dda81e5111895a0f
El-Zieny <abdoelzieny72@gmail.com>
2023-12-12 07:09:13 +0200

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
 if (argv.length < 4) {
-    console.log(0);
+  console.log(0);
 } else {
-    let array = argv;
-    array.splice(0, 2);
-    for (let i = 0; i < array.length; i++) {
-        array[i] = parseInt(array[i]);
-    }
-    array = array.sort();
-    array = array.reverse();
-    console.log(array[1]);
+  let array = argv;
+  array.splice(0, 2);
+  for (let i = 0; i < array.length; i++) {
+    array[i] = parseInt(array[i]);
+  }
+  array = array.sort();
+  array = array.reverse();
+  console.log(array[1]);
 }
