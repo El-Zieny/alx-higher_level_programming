b4b136b97e92cc744a4ff50ab481c7571ebf8bb5
El-Zieny <abdoelzieny72@gmail.com>
2023-12-13 16:30:19 +0200

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  function print () {
+    for (let i = 0; i < h; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
